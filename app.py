d08e742f5eeea482c58daad98b5722b4dc44b9f5
qinelin <83345815+qinelin@users.noreply.github.com>
2023-08-27 00:45:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,11 +29,11 @@ channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 handler = WebhookHandler(channel_secret)
 channel_access_token = os.getenv('LINE_ACCESS_TOKEN', None)
 if channel_secret is None:
-print('Specify LINE_CHANNEL_SECRET as environment variable.')
-sys.exit(1)
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
 if channel_access_token is None:
-print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-sys.exit(1)
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
 line_bot_api = LineBotApi(channel_access_token)
 # parser = WebhookParser(channel_secret)
 
